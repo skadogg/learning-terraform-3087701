f4c794eb607f22c56307c7b820763d0231d39cab
skadogg <wesanderson@gmail.com>
2025-09-10 13:11:17 -0500

    Enable NAT gateway in VPC module configuration

--- main.tf
+++ main.tf
@@ -25,8 +25,9 @@ module "blog_vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  # azs             = ["us-east-2-zg-1"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
 
   tags = {
     Terraform = "true"
