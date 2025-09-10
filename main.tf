0fb5753eee877a1f6921a7dea42bf63df308bfb7
skadogg <wesanderson@gmail.com>
2025-09-10 14:25:28 -0500

    Refactor VPC and autoscaling module configurations for consistency and clarity

--- main.tf
+++ main.tf
@@ -14,19 +14,16 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
-
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  # azs             = ["us-east-2-zg-1"]
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
 
   tags = {
     Terraform = "true"
@@ -34,20 +31,20 @@ module "blog_vpc" {
   }
 }
 
-module "autoscaling" {
+
+module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "9.0.1"
+  version = "6.5.2"
 
   name = "blog"
-  min_size = 1
-  max_size = 2
 
+  min_size            = 1
+  max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
-  security_groups = [module.blog_sg.security_group_id]
-
-  image_id      = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
@@ -86,14 +83,12 @@ module "blog_alb" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.3.0"
-  name = "blog"
+  version = "4.13.0"
 
-  vpc_id = module.blog_vpc.vpc_id
-
-  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  vpc_id  = module.blog_vpc.vpc_id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules  = ["all-all"]
+  egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
